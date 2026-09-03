49a715ef7def12f337d7b9547750a0753a45c890
Robin Richard-Canavaggio <22174078+r-richardcanavaggio@users.noreply.github.com>
2026-09-03 11:50:52 +0200

    ex02 done

---
+++ ex02/callLimit.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+
+def callLimit(limit: int):
+    count = 0
+
+    def callLimiter(function):
+        def limit_function(*args: Any, **kwargs: Any):
+            nonlocal count
+
+            if count < limit:
+                count += 1
+                return function(*args, **kwargs)
+            else:
+                print(f"Error: {function} call too many times")
+        return limit_function
+    return callLimiter
